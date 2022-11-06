cc2d7be75837810b0be9a5498c73c6ea90cf326d
Dariusz Jendryczka <88456245+DJendryczka@users.noreply.github.com>
2022-11-06 08:08:02 +0100

    Add overlay to splash image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,45 @@
-import React from 'react'
+import React from 'react';
 import { useState } from 'react';
-import './Navbar.css'
-import { Link } from 'react-router-dom'
+import './styles/Navbar.css'
+import { Link } from 'react-router-dom';
 import ReorderOutlinedIcon from '@mui/icons-material/ReorderOutlined';
 function Navbar() {
+  const [openLinks, setOpenLinks] = useState(false);
 
-    const [openLinks, setOpenLinks] = useState(false)
-
-    const toggleButton = () => {
-        setOpenLinks(!openLinks)
-    }
-    //
+  const toggleButton = () => {
+    setOpenLinks(!openLinks);
+  };
+  //
   return (
-    <div className='navbar'>
+    <div>
+      <div className='navbar'>
         <div className='left-side'>
-            <Link to='/' className='logo className='>LOGO</Link>
+          <Link to='/' className='logo className='>
+            LOGO
+          </Link>
         </div>
         <div className='right-side'>
-           <div className='links'>
-           <Link to='/'>HEM</Link>
-           <Link to='/About'>OM OSS</Link>
-           <Link to='/Services'>TJÄNSTER</Link>
-           <Link to='/Projects'>PROJEKT</Link>
-           <Link to='/Contact'>KONTAKT</Link>
-           </div>
-           <button  onClick={toggleButton}>
+          <div className='links'>
+            <Link to='/'>HEM</Link>
+            <Link to='/About'>OM OSS</Link>
+            <Link to='/Services'>TJÄNSTER</Link>
+            <Link to='/Projects'>PROJEKT</Link>
+            <Link to='/Contact'>KONTAKT</Link>
+          </div>
+          <button onClick={toggleButton}>
             <ReorderOutlinedIcon />
-           </button>
-           <div className='hidden' id={openLinks ? 'open' : 'close'} >
-           <Link to='/'>HEM</Link>
-           <Link to='/About'>OM OSS</Link>
-           <Link to='/Services'>TJÄNSTER</Link>
-           <Link to='/Projects'>PROJEKT</Link>
-           <Link to='/Contact'>KONTAKT</Link>
-           </div>
+          </button>
+          <div className='hidden' id={openLinks ? 'open' : 'close'}>
+            <Link to='/'>HEM</Link>
+            <Link to='/About'>OM OSS</Link>
+            <Link to='/Services'>TJÄNSTER</Link>
+            <Link to='/Projects'>PROJEKT</Link>
+            <Link to='/Contact'>KONTAKT</Link>
+          </div>
         </div>
-      
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,17 @@
-import React from 'react'
-
+import React from 'react';
+import '../styles/Home.css';
+import SplashImg from '../../assets/car.mp4';
 const Home = () => {
   return (
-    <div>
-        <h1>Home</h1>
+    <div className='container'>
+      <video src={SplashImg} autoPlay loop muted />
+      <div className='overlay'></div>
+      <div className='header'>
+        <h1>Välkommen till RKBilskadecenter AB</h1>
+        <h2>Din lokala bilskadereparatör</h2>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
