364eb245e876e3650f62d851a373e4ea1d47c325
Dariusz Jendryczka <88456245+DJendryczka@users.noreply.github.com>
2022-11-07 20:47:20 +0100

    Add styling to about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,25 +1,27 @@
-import React from 'react'
-import '../styles/About.css'
+import React from 'react';
+import '../styles/About.css';
 import Aboutimg from '../../assets/rkfront.jpg';
 
 const About = () => {
   return (
-    <div>
-      <div className='container'>
-        <div className='box'>
+    <div className='container'>
+      <div className='container-box'>
+        <div className='img-box'>
           <img src={Aboutimg} alt='RK Bil entrance' className='img' />
         </div>
         <div className='box'>
-        <h1>Om Oss</h1>
-            <p className='about-text'>RK Bilskadecenter är ett relativt ny startad företag, dock med många års erfaranhet inom bilplåtskador.
-              Vi följer alltid fabrikantenss anvisningar och uppdaterar vår kunskap om dessa löpande.
-              Vi åtar oss stora så som små uppdrag.
-              Någgrannhet och snabba leveranser är vår starka sida.
+          <h1 className='about-tittle'>Om Oss</h1>
+          <p className='about-text'>
+            RK Bilskadecenter är ett relativt ny startad företag, dock med många
+            års erfaranhet inom bilplåtskador. Vi följer alltid fabrikantenss
+            anvisningar och uppdaterar vår kunskap om dessa löpande. Vi åtar oss
+            stora så som små uppdrag. Någgrannhet och snabba leveranser är vår
+            starka sida.
           </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,11 +3,11 @@ import '../styles/Home.css';
 import SplashImg from '../../assets/car.mp4';
 const Home = () => {
   return (
-    <div className='container'>
+    <div className='container-home'>
       <video src={SplashImg} autoPlay loop muted />
       <div className='overlay'></div>
       <div className='header'>
-        <h1>Välkommen till <br/><span className='company-name'>RK Bilskadecenter AB</span></h1>
+        <h1 className='splash-text'>Välkommen till <br/><span className='company-name'>RK Bilskadecenter AB</span></h1>
         <h2>Din lokala bilskadereparatör</h2>
       </div>
     </div>
